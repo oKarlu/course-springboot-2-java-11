65a15a07c43036e3342becec167c86c28bacc6de
Carlos Marinho <cadumsantos@hotmail.com>
2022-05-01 09:10:19 -0300

    H2 database, test profile, JPA

--- src/main/java/com/cadu/course/entities/User.java
+++ src/main/java/com/cadu/course/entities/User.java
@@ -2,12 +2,24 @@ package com.cadu.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name, email, phone, password;
+	
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
 	
 	public User() {
 	}
@@ -84,8 +96,4 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
-	
 }
